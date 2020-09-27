c62c271141d72419ef88494a9456fad980e75115
slinksoft <slinkpro@gmail.com>
2020-09-26 20:34:17 -0400

    edit in gameroom credits

--- src/org/emulinker/kaillera/model/impl/KailleraGameImpl.java
+++ src/org/emulinker/kaillera/model/impl/KailleraGameImpl.java
@@ -490,7 +490,7 @@ public final class KailleraGameImpl implements KailleraGame
 			announce("Type /p2pon to ignore ALL server activity during gameplay.", user);
 			announce("This will reduce lag that you contribute due to a busy server.", user);
 			announce("NOTE: This server is using EmuLinker X" +  " v" + getServer().getReleaseInfo().getVersionString(), user);
-			announce("By: Slink, OGenDroX, Firo, and Agent 21 iXi ", user);
+			announce("By: Firo, Agent 21 iXi, Slink, and OGenDroX ", user);
 			announce("************************", user);
 			/*
 			if(autoFireDetector != null)
